620cc5f4fe55aecc82aa00cc72a496299ef698e1
Rifat <mdrifatahmed787@gmail.com>
2022-12-03 12:55:03 +0600

    last commit

--- index.js
+++ index.js
@@ -137,6 +137,7 @@ async function run() {
       res.send(result);
     });
 
+    //delete reviews
     app.delete("/reviews/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
